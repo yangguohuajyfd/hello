0707723f5f820804c68dfe1c55d4c76419b74047
yanghongnian <1024813650@qq.com>
2018-03-15 17:26:04 +0800

    第一次 提交 eclipse

---
+++ src/main/java/hello/gello.java
@@ -0,0 +1,17 @@
+package hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class gello {
+	
+	private String id;
+	private String name;
+
+	
+}
